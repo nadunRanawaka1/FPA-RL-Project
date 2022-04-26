92a7e61e05a77be6631ee69f7fca5ab45849f4fb
Nadun Ranawaka <nadun.ranawaka@gatech.edu>
2022-04-26 01:30:11 -0400

    Agent can walk around but doesnt find goal

--- learner.py
+++ learner.py
@@ -27,10 +27,10 @@ class Learner:
 		self.kernelSizes = [5,5,5] #size of kernel for each conv layer (CNN)
 		self.K = 32  # Number of samples in our minibatch
 		self.I = 20000  # Number of gradient descent iterations
-		self.T = 20# Max time steps for the exploration (i.e., policy roll-out)
+		self.T = 50# Max time steps for the exploration (i.e., policy roll-out)
 		self.episodes = 10000 #number of training episodes to run
 		self.gamma = 0.95  # discount factor
-		self.epsilon = 1 #episilon for epsilon-greedy rollouts
+		self.epsilon = 0.7 #episilon for epsilon-greedy rollouts
 		self.epsilon_o = 0.05  # Minimum amount of noise to maintain in the epsilon-greedy policy rollouts
 		self.epsilon_decay_const = 1e-4  # Rate at which epsilon is annealed from 1 to epsilon_O
 		self.numInputDims = 5 #how many features for each state
@@ -40,6 +40,7 @@ class Learner:
 		self.act = [0,1,2,3] #list of actions as follows [move right 1 pixel, move down 1, move left 1
 									 # move up 1, move right 2, move down 2 ...	]
 		self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+		self.batchSize = 5 #batchsize when optimizing the model
 
 
 	def run_learner(self):
@@ -50,13 +51,15 @@ class Learner:
 			self.numOutputDims, CNN = True, kernels = self.kernelSizes).to(device) 
 		target_net.load_state_dict(policy_net.state_dict())
 		target_net.eval()
+		optimizer = optim.RMSprop(policy_net.parameters())
+
 		
 
 		###IMPORTANT: In the img array indices are (y,x)
 		start = (60, 120)
 		goal =  (490, 440)
 
-		map_path  = "Map_1.png"
+		map_path  = "Map_1_obs.png"
 		map = plt.imread(map_path)
 		
 		
@@ -77,24 +80,29 @@ class Learner:
 		replay_buffer = ReplayMemory(10000)
 		curr_pos = start #setting the current location
 		curr_state = t_map #the current state is the map/env
+
+		at_goal = False 
 		for i in range(self.episodes):
 			print("starting Episode: {}".format(i))
 			for j in range(self.T):
 				sample = random.random()
 				if sample < self.epsilon:
-					print("Taking random action")
+					# print("Taking random action")
 					action = random.choice(self.act)
 				else:
 					with torch.no_grad():
 						q_vals = policy_net(curr_state) #q_vals of current state
-					print("taking best action")
+					# print("taking best action")
 					action = (torch.argmax(q_vals)).int()
 				print("action is: {}".format(action))
 				r, map, next_pos = transition(map, curr_pos, goal, action)
+				if (r == 10):
+					at_goal = True
+					break
 				print("moving to: {}".format(next_pos))
-				print(map[next_pos[0],next_pos[1],:])
+			
 
-				action = torch.tensor([[action]], device=device)
+				action = torch.tensor([[action]], device=device, dtype=torch.int64)
 				r = torch.tensor([r], device=device)
 				#updating map for torch format
 				t_map = map.reshape(4,600,600) #t_map = torch_map, map in format for pytorch
@@ -105,25 +113,31 @@ class Learner:
 				replay_buffer.push(curr_state, action, t_map, r)
 				curr_pos = next_pos
 				curr_state = t_map
-				policy_net  = self.optimize_model(replay_buffer, policy_net, target_net)
+				
 				self.epsilon = self.epsilon - self.epsilon_decay_const	
 
+			if (at_goal):
+				print("We Reached the goal")
+				break
 				
+			policy_net  = self.optimize_model(replay_buffer, policy_net, target_net, optimizer)
 			
-			target_net.load_state_dict(policy_net.state_dict()) #Q_{i+1}(s,a)
-			if (i % 100 == 0):
+			if (i % 15 == 0):
+				target_net.load_state_dict(policy_net.state_dict()) #Q_{i+1}(s,a)
 				print("Epsilon is: {}".format(self.epsilon))
 				print("size of replay_buffer is: {}".format(len(replay_buffer)))
+				
+			if (i % 59 == 0):
 				imgplot = plt.imshow(map)
 				plt.show()
 
-
-	def optimize_model(self, memory, policy_net, target_net):
+		imgplot = plt.imshow(map)
+		plt.show()
+	def optimize_model(self, memory, policy_net, target_net, optimizer):
 	    if len(memory) < self.T:
 	        return policy_net
-	    transitions = memory.sample(self.T)
+	    transitions = memory.sample(self.batchSize)
 	    device = self.device
-	    optimizer = optim.RMSprop(policy_net.parameters())
 	    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
 	    # detailed explanation). This converts batch-array of Transitions
 	    # to Transition of batch-arrays.
@@ -131,13 +145,15 @@ class Learner:
 
 	    # Compute a mask of non-final states and concatenate the batch elements
 	    # (a final state would've been the one after which simulation ended)
-	    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-	                                          batch.next_state)), device=device, dtype=torch.bool)
-	    non_final_next_states = torch.cat([s for s in batch.next_state
-	                                                if s is not None])
+	    # Currently not needed, might need if we change the environment
+	    # non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+	    #                                       batch.next_state)), device=device, dtype=torch.bool)
+	    # non_final_next_states = torch.cat([s for s in batch.next_state
+	    #                                             if s is not None])
 	    state_batch = torch.cat(batch.state)
 	    action_batch = torch.cat(batch.action)
 	    reward_batch = torch.cat(batch.reward)
+	    next_state_batch = torch.cat(batch.next_state)
 
 	    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
 	    # columns of actions taken. These are the actions which would've been taken
@@ -149,8 +165,12 @@ class Learner:
 	    # on the "older" target_net; selecting their best reward with max(1)[0].
 	    # This is merged based on the mask, such that we'll have either the expected
 	    # state value or 0 in case the state was final.
-	    next_state_values = torch.zeros(self.T, device=device)
-	    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+	    # next_state_values = torch.zeros(self.T, device=device)
+	    # next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+
+
+	    next_state_values = target_net(next_state_batch).max(1)[0].detach() #we compute Q_vals for the next state and pick the max
+	    
 	    # Compute the expected Q values
 	    expected_state_action_values = (next_state_values * self.gamma) + reward_batch
 
--- mdp.py
+++ mdp.py
@@ -8,14 +8,32 @@ actions = [(1,0), (0,1), (-1,0), (0,-1)]
 def transition(map, curr, goal, action): 
 	move = actions[action]
 	new_pos = (curr[0] + move[0], curr[1] + move[1])
+
 	if (new_pos == goal): #we are at goal
 		map[goal,0:3] = 0.0
 		return 10, map, new_pos
+
+	elif (new_pos[0] < 0 or new_pos[1] < 0 or new_pos[0] > 599 or new_pos[1] > 599): #we went off the map
+		return -2, map, curr
+
 	pixel = map[new_pos[0], new_pos[1], :]
+
+	mhat_dist_old = abs(goal[0] - curr[0]) + abs(goal[1] - curr[1]) #calculating the manhattan distance from the old position to goal
+	mhat_dist_new = abs(goal[0] - new_pos[0]) + abs(goal[1] - new_pos[1]) #same for new position
+
+	euc_dist_old = ((goal[0] - curr[0])**2 + (goal[1] - curr[1])**2)**0.5 #calculating euclidean distance between goal and old pos
+	euc_dist_new = ((goal[0] - new_pos[0])**2 + (goal[1] - new_pos[1])**2)**0.5
+
 	if (pixel[0] == 0.0): #we hit the path, TODO: fix this with something better
 		return -2, map, new_pos
+
 	elif (pixel[0] != 1.): #we hit something else, like an obstacle
 		return -5, map, curr
+
+	elif (mhat_dist_new < mhat_dist_old): #the new position is closer to the goal
+		map[new_pos[0],new_pos[1], 0:3] = 0.0
+		return -0.05, map, new_pos
+
 	else: #mark path, small negative reward for moving
 		map[new_pos[0],new_pos[1], 0:3] = 0.0
 		return -0.1, map, new_pos
