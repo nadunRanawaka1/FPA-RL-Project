7330b1748e1b255e4e1a8b1d8d4a311fcf21f983
Nadun Ranawaka <nadun.ranawaka@gatech.edu>
2022-04-30 15:57:29 -0400

    slight changes to learner conventions

--- learner.py
+++ learner.py
@@ -24,14 +24,14 @@ from envs.path_plan_env import PathPlanEnv
 class Learner:
 	def __init__(self):
 
-		self.alpha = 2e-2  # Learning rate for all gradient descent methods
+		self.alpha = 2e-3  # Learning rate for all gradient descent methods
 		self.numNodesPerLayer = [32]  # Vector of the number of hidden nodes per layer, indexed from input -> output
 		self.outChannelsPerLayer = [16, 32, 32] #Number of channels per output layer (CNN)
 		self.kernelSizes = [5,5,5] #size of kernel for each conv layer (CNN)
 		self.K = 32  # Number of samples in our minibatch
 		self.I = 20000  # Number of gradient descent iterations
 		self.T = 100# Max time steps for the exploration (i.e., policy roll-out)
-		self.episodes = 10000 #number of training episodes to run
+		self.opt_iter = 10000 #max number of optimization iterations
 		self.gamma = 0.95  # discount factor
 		self.epsilon = 0.9 #epsilon for epsilon-greedy rollouts
 		self.epsilon_o = 0.05  # Minimum amount of noise to maintain in the epsilon-greedy policy rollouts
@@ -48,6 +48,7 @@ class Learner:
 		self.episodeIter = 0 #number of iterations/moves per episode
 		self.stepsToGoal = [] #number of steps to goal for each episode
 		self.successEpis = 0 #number of episodes where the agent found the goal
+		self.totalEpis = 0 #total number of episodes we have run
 
 
 	def run_learner(self):
@@ -59,7 +60,7 @@ class Learner:
 		target_net.load_state_dict(policy_net.state_dict())
 		target_net.eval()
 		# optimizer = optim.RMSprop(policy_net.parameters())
-		optimizer = optim.Adam(policy_net.parameters())
+		optimizer = optim.Adam(policy_net.parameters(), lr=self.alpha)
 
 
 		#TODO: keep window of observations and pass into NN, each sample in window = 1 channel
@@ -68,36 +69,25 @@ class Learner:
 		start = [120, 120]
 		goal =  [450, 440]
 
-		env: PathPlanEnv = gym.make("envs/PathPlanEnv-v0", file="Map_1_obs.png", start=np.array(start), goal=np.array(goal))
-
-		# map_path  = "Map_1_obs.png"
-		# map = plt.imread(map_path)
+		env: PathPlanEnv = gym.make("envs/PathPlanEnv-v0", file="maps/Map_7_obs.png", start=np.array(start), goal=np.array(goal))
 
 		obs = env.reset()
 		plt.imshow(obs["map"], cmap="gray")
 		plt.show()
 
 		print(obs["map"].shape)
-		
-		# map[start, 0:3] = 0.0 #marking start on map
-		# plt.figure()
-		# imgplot = plt.imshow(map)
-		# plt.show()
+
 
 		# t_map = map.reshape(4,600,600) #t_map = torch_map, map in format for pytorch
 		t_map, curr_state, curr_pos = self.reset_vars(env)
-		# t_map = t_map.cuda()
 
 
 		#Will run reinforcement learning below
 
-		replay_buffer = ReplayMemory(10000)
-		# curr_pos = env.current_position #setting the current location
-		# curr_state = t_map #the current state is the map/env
+		replay_buffer = ReplayMemory(10000) #this will hold a buffer of <s,a,r,s'>
 
-		at_goal = False 
-		for i in range(self.episodes):
-			print("starting Episode: {}".format(i))
+		for i in range(self.opt_iter):
+
 			for j in range(self.T):
 				sample = random.random()
 				if sample < self.epsilon:
@@ -109,12 +99,10 @@ class Learner:
 
 					# print("taking best action")
 					action = (torch.argmax(q_vals)).int().item()
+
 				# print("action is: {}".format(action))
 				# r, map, next_pos = transition(map, curr_pos, goal, action)
 				obs, reward, done, _ = env.step(action)
-
-				# print("moving to: {}, reward: {}".format(env.current_position, reward))
-			
 
 				action = torch.tensor([[action]], device=device, dtype=torch.int64)
 				reward = torch.tensor([reward], device=device)
@@ -137,35 +125,38 @@ class Learner:
 					break
 
 			if (done):
-				print("We Reached the goal. Total number of iterrations: {}".format(self.episodeIter))
-				# imgplot = plt.imshow(t_map.squeeze(0).squeeze(0).detach().numpy(), cmap="gray")
-				# plt.show()
+				print("We Reached the goal. Total number of iterations: {}".format(self.episodeIter))
 				self.stepsToGoal.append(self.episodeIter)
 				self.successEpis += 1
 				if (self.successEpis > 50):
 					break
 				obs = env.reset()
 				t_map, curr_state, curr_pos = self.reset_vars(env)
+				self.totalEpis += 1
+				print("starting Episode: {}".format(self.totalEpis))
 
 			if (self.episodeIter > 6000):
+				print("Agent failed to find goal")
 				t_map, curr_state, curr_pos = self.reset_vars(env)
+				self.totalEpis += 1
+				print("starting Episode: {}".format(self.totalEpis))
 
 			policy_net  = self.optimize_model(replay_buffer, policy_net, target_net, optimizer)
+
 			
 			if (i % 5 == 0):
 				target_net.load_state_dict(policy_net.state_dict()) #Q_{i+1}(s,a)
 
-				
-			# if (i % 10 == 0):
-			# 	imgplot = plt.imshow(t_map.squeeze(0).squeeze(0).detach().numpy(), cmap="gray")
-			# 	plt.show()
 		plt.clf()
 		plt.plot(self.stepsToGoal)
 		plt.xlabel("Episode")
 		plt.ylabel("Steps to goal")
 		plt.xlim(0, 53)
+		plt.title("lr: {} ,gamma: {}, eps-decay: {}".format(self.alpha, self.gamma, self.epsilon_decay_const))
 		plt.show()
 
+
+	###This function resets a few variables needed for the training loop
 	def reset_vars(self,env):
 		self.episodeIter = 0
 		obs = env.reset()
@@ -201,8 +192,6 @@ class Learner:
 		next_state_batch = torch.cat(batch.next_state)
 
 
-		# print(torch.cuda.memory_summary())
-		print(torch.cuda.memory_allocated())
 		# Compute Q(s_t, a) - the model computes Q(s_t), then we select the
 		# columns of actions taken. These are the actions which would've been taken
 		# for each batch state according to policy_net
@@ -215,13 +204,11 @@ class Learner:
 		# state value or 0 in case the state was final.
 		# next_state_values = torch.zeros(self.T, device=device)
 		# next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
-		print(torch.cuda.memory_allocated())
 
 		next_state_values = target_net(next_state_batch).max(1)[0].detach() #we compute Q_vals for the next state and pick the max
 
 		# Compute the expected Q values
 		expected_state_action_values = (next_state_values * self.gamma) + reward_batch
-		print(torch.cuda.memory_allocated())
 		# Compute Huber loss
 		criterion = nn.SmoothL1Loss()
 		loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
